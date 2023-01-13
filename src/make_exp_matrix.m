76cd4bf00a9c006c040a69e903257bd448871d63
Allison Schiffman <schiffman@ucla.edu>
2023-01-13 14:05:30 -0800

    exp matrix match obs figure

---
+++ src/make_exp_matrix.m
@@ -0,0 +1,9 @@
+exp_matrix.irf = [0.1, 0.25, 0.01, 0.75, 1, 0.075, 0];
+exp_matrix.nfkb = [1,   0,    1,    0.5, 0, 0.5, 0.5];
+exp_matrix.ifnb = [0.4, 0.2,  0,    1,   1, 0.4, 0];
+
+exp_matrix_old.irf = [0.1000 0.2000 0.0100 0.6000 0.8000 0.1000 0];
+exp_matrix_old.nfkb = [0.5000 0 1 0.5000 0 1 1];
+exp_matrix_old.ifnb = [0.4000 0.2000 0 1 1 0.4000 0];
+
+save(['../data/exp_matrix_norm.mat'], 'exp_matrix')
