c1031eeaf76307966613754ba0bef79ad9c86e3b
aschiffman <schiffman@ucla.edu>
2023-04-12 18:43:19 -0700

    Made additional plots for comparing params/residuals

--- src/plot_code/compare_3_site_model.R
+++ src/plot_code/compare_3_site_model.R
@@ -27,16 +27,17 @@ p=ggplot(res_long, aes(x=model,y=residual)) +
 ggsave(p, filename = str_c("../3-site-model/figs/all_models_all_residuals.png"),
        height = 3, width = 6)
 
-# #### RMSD plot ####
-# #  Normalizing values to model 1 for each dataset
-# rmsd_1 = read_csv(str_c("../data/ModelB","1","_rmsd.csv"),col_names = c("rmsd1","dset"))
-# rmsd_2 = read_csv(str_c("../data/ModelB","2","_rmsd.csv"),col_names = c("rmsd2","dset"))
-# rmsd_3 = read_csv(str_c("../data/ModelB","3","_rmsd.csv"),col_names = c("rmsd3","dset"))
-# rmsd_4 = read_csv(str_c("../data/ModelB","4","_rmsd.csv"),col_names = c("rmsd4","dset"))
-# 
-# rmsd=full_join(rmsd_1,rmsd_2, by="dset")
-# rmsd=full_join(rmsd,rmsd_3, by="dset")
-# rmsd=full_join(rmsd,rmsd_4, by="dset")
+#### RMSD plot ####
+rmsd_1 = read_csv(str_c("../data/ModelB","1","_rmsd.csv"),col_names = c("rmsd1","dset"))
+rmsd_2 = read_csv(str_c("../data/ModelB","2","_rmsd.csv"),col_names = c("rmsd2","dset"))
+rmsd_3 = read_csv(str_c("../data/ModelB","3","_rmsd.csv"),col_names = c("rmsd3","dset"))
+rmsd_4 = read_csv(str_c("../data/ModelB","4","_rmsd.csv"),col_names = c("rmsd4","dset"))
+
+rmsd=full_join(rmsd_1,rmsd_2, by="dset")
+rmsd=full_join(rmsd,rmsd_3, by="dset")
+rmsd=full_join(rmsd,rmsd_4, by="dset")
+
+# # Normalizing values to model 1 for each dataset
 # rmsd_rel = rmsd %>%
 #   mutate("rmsd2"=rmsd2/rmsd1,
 #          "rmsd3"=rmsd3/rmsd1,
@@ -48,13 +49,69 @@ ggsave(p, filename = str_c("../3-site-model/figs/all_models_all_residuals.png"),
 # ggplot(rmsd_rel, aes(x=model,y=rmsd)) +
 #   geom_boxplot() +
 #   theme_bw()
-# 
-# 
-# rmsd_all = rmsd %>%
-#   pivot_longer(cols=c("rmsd1","rmsd2","rmsd3","rmsd4"),names_to = "model",
-#                values_to = "rmsd")
-# 
-# ggplot(rmsd_all, aes(x=model,y=rmsd)) +
-#   geom_boxplot() +
-#   theme_bw()
 
+
+rmsd_all = rmsd %>%
+  pivot_longer(cols=c("rmsd1","rmsd2","rmsd3","rmsd4"),names_to = "model",
+               values_to = "rmsd") %>%
+  mutate(aic = case_when(
+    model == "rmsd1" ~ 7*log(rmsd^2)+2*6,
+    model == "rmsd4" ~  7*log(rmsd^2)+2*8,
+    TRUE ~  7*log(rmsd^2)+2*7))
+
+
+p= ggplot(rmsd_all, aes(x=model,y=rmsd)) +
+  geom_boxplot(outlier.size = 3) +
+  geom_jitter(data=rmsd_all[rmsd_all$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
+  geom_point(data=rmsd_all[rmsd_all$dset==0,], color="red",size=3)+
+  theme_bw()+
+  scale_x_discrete(labels=c(bquote("Model "*beta*"1"),
+                            bquote("Model "*beta*"2"),
+                            bquote("Model "*beta*"3"),
+                            bquote("Model "*beta*"4"))) +
+  ggtitle("All models RMSD")
+ggsave(p, filename = str_c("../3-site-model/figs/all_models_RMSD.png"),
+       height = 4, width = 4)
+
+p= ggplot(rmsd_all, aes(x=model,y=aic)) +
+  geom_boxplot(outlier.size = 3) +
+  geom_jitter(data=rmsd_all[rmsd_all$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
+  geom_point(data=rmsd_all[rmsd_all$dset==0,], color="red",size=3)+
+  theme_bw()+
+  scale_x_discrete(labels=c(bquote("Model "*beta*"1"),
+                            bquote("Model "*beta*"2"),
+                            bquote("Model "*beta*"3"),
+                            bquote("Model "*beta*"4"))) +
+  ggtitle("All models AIC")
+ggsave(p, filename = str_c("../3-site-model/figs/all_models_AIC.png"),
+       height = 4, width = 4)
+
+# Parameters
+read_parameters = function(model_name){
+  t_pars = c("t1","t2","t3","t4","t5","t6")
+  if (model_name=="1") {
+    param_names = t_pars
+    
+  } else if (model_name=="2") {
+    param_names = c("KI2",t_pars)
+    
+  } else if (model_name=="3") {
+    param_names = c("C",t_pars)
+    
+  } else if (model_name=="4") {
+    param_names = c("KI2","C",t_pars)
+    
+  }
+  df = read_csv(str_c("../data/ModelB",model_name,"_parameters.csv"),col_names = c(param_names,"dset"),
+                show_col_types = FALSE) %>%
+    pivot_longer(param_names, names_to = "parameter",values_to = "val") %>%
+    mutate(par_type = case_when(
+      parameter %in% t_pars ~ "t_parameter",
+      TRUE ~ "Other"))
+  return(df)
+}
+
+params_1 = read_parameters("1")
+params_2 = read_parameters("2")
+params_3 = read_parameters("3")
+params_4 = read_parameters("4")
--- src/plot_code/plot_3sitemodel_any_model.R
+++ src/plot_code/plot_3sitemodel_any_model.R
@@ -9,28 +9,24 @@ model_name=str_extract(model_name,"\\d")
 
 # get parameter names from model number
 t_pars = c("t1","t2","t3","t4","t5","t6")
+par_names=c("t1: IRF","t2: IRF",expression("t3: NF"*kappa*"B"),
+            "t4: IRF/IRF",expression("t5: IRF/NF"*kappa*"B"),
+            expression("t6: IRF/NF"*kappa*"B"))
 
 if (model_name=="1") {
   param_names = t_pars
-  par_names=c("t1: IRF","t2: IRF",expression("t3: NF"*kappa*"B"),
-              "t4: IRF/IRF",expression("t5: IRF/NF"*kappa*"B"),
-              expression("t6: IRF/NF"*kappa*"B"))
+
 } else if (model_name=="2") {
   param_names = c("KI2",t_pars)
-  par_names=c("t1: IRF","t2: IRF",expression("t3: NF"*kappa*"B"),
-              "t4: IRF/IRF",expression("t5: IRF/NF"*kappa*"B"),
-              expression("t6: IRF/NF"*kappa*"B"))
+
 } else if (model_name=="3") {
   param_names = c("C",t_pars)
-  par_names=c("t1: IRF","t2: IRF",expression("t3: NF"*kappa*"B"),
-              "t4: IRF/IRF",expression("t5: IRF/NF"*kappa*"B"),
-              expression("t6: IRF/NF"*kappa*"B"))
+
 } else if (model_name=="4") {
   param_names = c("KI2","C",t_pars)
-  par_names=c("t1: IRF","t2: IRF",expression("t3: NF"*kappa*"B"),
-              "t4: IRF/IRF",expression("t5: IRF/NF"*kappa*"B"),
-              expression("t6: IRF/NF"*kappa*"B"))
+
 }
+nparams = length(param_names)
 
 # Load data
 rmsd_df = read_csv(str_c("../data/ModelB",model_name,"_rmsd.csv"),col_names = c("rmsd","dset"))
@@ -54,7 +50,7 @@ dpt_names=c("WT_LPS",expression("NF"*kappa*"B KO LPS"),
             "IRF 2KO LPS","WT pIC",expression("NF"*kappa*"B KO pIC"),
             "IRF 2KO pIC", "IRF 3KO pIC")
 
-eval_df = rmsd_df %>% mutate(aic = log(rmsd^2)*7+2*6) %>%
+eval_df = rmsd_df %>% mutate(aic = 7*log(rmsd^2)+2*nparams) %>%
   pivot_longer(c("rmsd","aic"), names_to = "measurement",values_to = "val")
 
 dummy=bind_cols(dset=c(1,2,3,4),measurement=c("aic","aic","rmsd","rmsd"),val=c(-35,-10,0,0.2))
@@ -77,6 +73,7 @@ ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_all_data_fits.p
 # val_names = c(`t1`="t1: IRF", `t2` = "t2: IRF", `t3` = expression("t3: NF"*kappa*"B"),
 #               `t4`="t4: IRF/IRF", `t5` = expression("t5: IRF/NF"*kappa*"B"), `t6` = expression("t6: IRF/NF"*kappa*"B"))
 
+# labels messed up
 p=ggplot(params_df,aes(x=parameter,y=val)) +
   geom_boxplot(outlier.size = 3) +
   # geom_jitter(data=eval_df[eval_df$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
@@ -89,6 +86,100 @@ p=ggplot(params_df,aes(x=parameter,y=val)) +
 
 ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_all_parameters.png"),
        height = 6, width = 9)
+
+
+# only t parameters
+params_df1 = params_df %>%
+  filter(parameter %in% t_pars)
+
+p=ggplot(params_df1,aes(x=parameter,y=val)) +
+  geom_boxplot(outlier.size = 3) +
+  # geom_jitter(data=eval_df[eval_df$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
+  geom_point(data=params_df1[params_df1$dset==0,], color="red",size=3)+
+  # ggforce::facet_row(~par_type, scales = "free", space = "free") +
+  # geom_blank(data=dummy) + 
+  theme_bw() +
+  scale_x_discrete(labels=par_names) +
+  ggtitle(bquote("Model "*beta*.(model_name)*" transcription parameters from best fit model for each data set"))
+
+ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_t_parameters_boxplot.png"),
+       height = 5, width = 6)
+
+# only non t-parameters
+if (model_name=="2") {
+  params_df2 = params_df %>%
+    filter(par_type=="Other") %>%
+    mutate(log_par = log10(val))
+  
+  p=ggplot(params_df2,aes(x=parameter,y=log_par)) +
+    # geom_boxplot(outlier.size = 3) +
+    geom_jitter(data=params_df2[params_df2$dset!=0,], color="black",size=3,width=rel(0.1))+
+    geom_point(data=params_df2[params_df2$dset==0,], color="red",size=3)+
+    # ggforce::facet_row(~par_type, scales = "free", space = "free") +
+    expand_limits(y=c(2,-7)) +
+    theme_bw() +
+    scale_x_discrete(labels=c(expression(K[I2]/K[I1]))) +
+    ggtitle(bquote("Model "*beta*.(model_name)*" other parameters"))
+  
+  ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_other_parameters_boxplot.png"),
+         height = 6, width = 3)
+} else if (model_name=="3"){
+  params_df2 = params_df %>%
+    filter(par_type=="Other")
+  
+  p=ggplot(params_df2,aes(x=parameter,y=val)) +
+    # geom_boxplot(outlier.size = 3) +
+    geom_jitter(data=params_df2[params_df2$dset!=0,], color="black",size=3,width=rel(0.1))+
+    geom_point(data=params_df2[params_df2$dset==0,], color="red",size=3)+
+    # ggforce::facet_row(~par_type, scales = "free", space = "free") +
+    # expand_limits(y=c(2,-7)) +
+    theme_bw() +
+    scale_x_discrete(labels=c("C")) +
+    ggtitle(bquote("Model "*beta*.(model_name)*" other parameters"))
+  
+  ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_other_parameters_boxplot.png"),
+         height = 6, width = 3)
+} else if (model_name=="4"){
+  params_df2 = params_df %>%
+    filter(par_type=="Other")  %>%
+    mutate(log_par = log10(val),
+           parameter = factor(parameter, levels = c("KI2","C")))
+  
+  p=ggplot(params_df2,aes(x=parameter,y=log_par)) +
+    # geom_boxplot(outlier.size = 3) +
+    geom_line(aes(group=dset), alpha = 0.5, color="grey") +
+    geom_point(data=params_df2[params_df2$dset!=0,], color="black",size=3)+
+    geom_point(data=params_df2[params_df2$dset==0,], color="red",size=3)+
+    expand_limits(y=c(2,-7)) +
+    theme_bw() +
+    # scale_x_continuous(labels=parameter) +
+    # scale_x_discrete(labels=c(expression(K[I2]/K[I1]),"C")) +
+    ggtitle(bquote("Model "*beta*.(model_name)*" other parameters"))
+  
+  ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_other_parameters_boxplot.png"),
+         height = 6, width = 3)
+  
+  params_df3 = params_df %>%
+    filter(par_type=="Other")  %>%
+    mutate(log_val = log10(val)) %>%
+    select(!val) %>%
+    pivot_wider(names_from = parameter, values_from = log_val)
+  
+  p=ggplot(params_df3,aes(x=KI2,y=C)) +
+    # geom_boxplot(outlier.size = 3) +
+    # geom_line(aes(group=dset), alpha = 0.5, color="grey") +
+    geom_point(data=params_df3[params_df3$dset!=0,], color="black",size=3)+
+    geom_point(data=params_df3[params_df3$dset==0,], color="red",size=3)+
+    theme_bw() +
+    # scale_x_continuous(labels=parameter) +
+    # scale_x_discrete(labels=c(expression(K[I2]/K[I1]),"C")) +
+    ggtitle(bquote("Model "*beta*.(model_name)*" other parameters, log 10"))
+  
+  ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_other_parameters_scatter.png"),
+         height = 6, width = 6)
+  
+}
+
 
 p=ggplot(resid_df,aes(x=data_point,y=residual)) +
   geom_boxplot(outlier.size = 3) +
@@ -104,14 +195,14 @@ ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_all_residuals.p
        height = 6, width = 9)
 
 
-all_df = read_csv(str_c("../data/ModelB",model_name,"_parameters.csv"),col_names = c(param_names,"dset")) %>%
-  full_join(rmsd_df,by="dset") %>% mutate(aic = log(rmsd^2)*7+2*6)
-all_df=all_df %>%
-  mutate(good = aic<quantile(all_df$aic,0.25),
-         bad = aic>quantile(all_df$aic,0.75),
+par_df = read_csv(str_c("../data/ModelB",model_name,"_parameters.csv"),col_names = c(param_names,"dset")) %>%
+  full_join(rmsd_df,by="dset") %>% mutate(aic = 7*log(rmsd^2)+2*nparams)
+all_df=par_df %>%
+  mutate(good = aic<quantile(par_df$aic,0.25),
+         bad = aic>quantile(par_df$aic,0.75),
          keep=good+bad) %>%
   filter(keep==1) %>%
-  pivot_longer(c("t1","t2","t3","t4","t5","t6"), names_to = "parameter",values_to = "val")
+  pivot_longer(all_of(t_pars), names_to = "parameter",values_to = "val")
 
 # all_df[all_df$good+all_df$bad,]
 
@@ -128,3 +219,24 @@ p=ggplot(all_df,aes(x=good,y=val)) +
 
 ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_best_worst_parameters.png"),
        height = 6, width = 9)
+
+# Parameter box plots
+t_params = par_df %>%
+  select(all_of(t_pars),dset) %>%
+  pivot_longer(all_of(t_pars), values_to = "val", names_to = "param") %>%
+  group_by(param) %>%
+  summarize(mean = mean(val), err = sd(val)/sqrt(n())) %>%
+  mutate(lower = pmax(mean-err,0),
+         upper=pmin(mean+err,1))
+
+p=ggplot(t_params, aes(x=param, y=mean)) +
+  geom_col()+
+  geom_errorbar(aes(ymin=lower,ymax=upper))+
+  theme_bw() +
+  expand_limits(y=1) +
+  scale_x_discrete(labels=par_names) +
+  ylab("Relative transcription activity") +
+  ggtitle(bquote("Model "*beta*.(model_name)))
+ggsave(p, filename = str_c("../3-site-model/figs/B",model_name,"_t_parameters_barplot.png"),
+       height = 5, width = 5)
+
