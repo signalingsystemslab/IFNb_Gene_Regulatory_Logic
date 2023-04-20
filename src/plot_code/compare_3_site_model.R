bda569eaca7ddc40fad9397cd89d9d2ed08e5b64
aschiffman <schiffman@ucla.edu>
2023-04-19 19:34:51 -0700

    add comparing worst residual

--- src/plot_code/compare_3_site_model.R
+++ src/plot_code/compare_3_site_model.R
@@ -27,6 +27,25 @@ p=ggplot(res_long, aes(x=model,y=residual)) +
 ggsave(p, filename = str_c("../3-site-model/figs/all_models_all_residuals.png"),
        height = 3, width = 6)
 
+# Least residual
+res_long_max= res_long %>%
+  group_by(dset,model) %>%
+  filter(abs(residual)==max(abs(residual)))
+
+jitterer = position_jitter(seed = 5,width=0.2) 
+p=ggplot(res_long_max, aes(x=model,y=residual^2)) +
+  geom_boxplot(outlier.shape = NA, fill="grey") +
+  geom_point(data=res_long_max[res_long_max$dset!=0,],position = jitterer, aes(color=data_point),size=2) +
+  geom_point(data=res_long_max[res_long_max$dset==0,], aes(color=data_point),size=2) +
+  scale_color_brewer(palette="Purples")+
+  geom_point(data=res_long_max[res_long_max$dset==0,], shape=1,
+             size=2,color="red") +
+  ggtitle("All models comparing worst fit point for each dataset") +
+  theme_bw()
+
+ggsave(p, filename = str_c("../3-site-model/figs/all_models_max_residuals.png"),
+       height = 3, width = 4)
+
 #### RMSD plot ####
 rmsd_1 = read_csv(str_c("../data/ModelB","1","_rmsd.csv"),col_names = c("rmsd1","dset"))
 rmsd_2 = read_csv(str_c("../data/ModelB","2","_rmsd.csv"),col_names = c("rmsd2","dset"))
@@ -85,6 +104,62 @@ p= ggplot(rmsd_all, aes(x=model,y=aic)) +
   ggtitle("All models AIC")
 ggsave(p, filename = str_c("../3-site-model/figs/all_models_AIC.png"),
        height = 4, width = 4)
+
+# Compare to 2-site model
+rmsd_two_exp = read_csv(str_c("../data/exp_data_mins.csv")) %>%
+  mutate(dset = 0)
+rmsd_two_site = read_csv(str_c("../data/syn_data_mins.csv")) %>%
+  mutate(dset = rep(c(1:99),length.out = n())) %>%
+  bind_rows(rmsd_two_exp) %>%
+  mutate(model = str_c("twosite",model)) %>%
+  rename("minRMSD" = "rmsd") %>%
+  dplyr::select(c("rmsd","dset","model"))
+
+rmsd_both_models = rmsd %>%
+  pivot_longer(cols=c("rmsd1","rmsd2","rmsd3","rmsd4"),names_to = "model",
+               values_to = "rmsd") %>%
+  bind_rows(rmsd_two_site) %>%
+  mutate(aic = case_when(
+    model == "rmsd1" ~ 7*log(rmsd^2)+2*6,
+    model == "rmsd4" ~  7*log(rmsd^2)+2*8,
+    str_detect(model, "^twosite") ~ 7*log(rmsd^2)+2*1,
+    TRUE ~  7*log(rmsd^2)+2*7))
+
+p= ggplot(rmsd_both_models, aes(x=model,y=rmsd)) +
+  geom_boxplot(outlier.size = 3) +
+  geom_jitter(data=rmsd_both_models[rmsd_both_models$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
+  geom_point(data=rmsd_both_models[rmsd_both_models$dset==0,], color="red",size=3)+
+  theme_bw()+
+  scale_x_discrete(labels=c(bquote("Model "*beta*"1"),
+                            bquote("Model "*beta*"2"),
+                            bquote("Model "*beta*"3"),
+                            bquote("Model "*beta*"4"),
+                            "2-site IRF",
+                            bquote("2-site NF"*kappa*"B"),
+                            "2-site AND",
+                            "2-site OR")) +
+  ggtitle("All models RMSD 2-site and 3-site") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave(p, filename = str_c("../3-site-model/figs/all_models_RMSD_2site_3site.png"),
+       height = 4, width = 5)
+
+p= ggplot(rmsd_both_models, aes(x=model,y=aic)) +
+  geom_boxplot(outlier.size = 3) +
+  geom_jitter(data=rmsd_both_models[rmsd_both_models$dset!=0,], color="black",alpha=0.3,size=3,width=rel(0.1))+
+  geom_point(data=rmsd_both_models[rmsd_both_models$dset==0,], color="red",size=3)+
+  theme_bw()+
+  scale_x_discrete(labels=c(bquote("Model "*beta*"1"),
+                            bquote("Model "*beta*"2"),
+                            bquote("Model "*beta*"3"),
+                            bquote("Model "*beta*"4"),
+                            "2-site IRF",
+                            bquote("2-site NF"*kappa*"B"),
+                            "2-site AND",
+                            "2-site OR")) +
+  ggtitle("All models AIC 2-site and 3-site")
+ggsave(p, filename = str_c("../3-site-model/figs/all_models_AIC_2site_3site.png"),
+       height = 4, width = 5)
+
 
 # Parameters
 read_parameters = function(model_name){
