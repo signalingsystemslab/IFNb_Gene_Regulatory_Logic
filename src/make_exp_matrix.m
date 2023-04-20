859ab6ef0da29a97d464e84d073d8e58855abc72
aschiffman <schiffman@ucla.edu>
2023-04-19 19:34:34 -0700

    save csv

--- src/make_exp_matrix.m
+++ src/make_exp_matrix.m
@@ -7,3 +7,8 @@ exp_matrix_old.nfkb = [0.5000 0 1 0.5000 0 1 1];
 exp_matrix_old.ifnb = [0.4000 0.2000 0 1 1 0.4000 0];
 
 save('./data/exp_matrix_norm.mat', 'exp_matrix')
+
+% Convert to matrix with columns for irf, nfkb, ifnb
+exp_matrix_mat = [exp_matrix.irf', exp_matrix.nfkb', exp_matrix.ifnb'];
+% Save as csv
+csvwrite('./data/exp_matrix_norm.csv', exp_matrix_mat)
