354a3b320a64562beb619e21da2e6337afa30348
aschiffman <schiffman@ucla.edu>
2023-07-27 10:56:14 -0700

    Entire simulation

--- src/simulation/simulate_isg_expression.py
+++ src/simulation/simulate_isg_expression.py
@@ -6,13 +6,40 @@ from isg_module import change_equations as isg_change_equations
 from ifnb_module import change_equations as ifnb_change_equations
 plt.style.use("~/IFN_paper/src/theme_bw.mplstyle")
 
-def get_inputs(N, I, P):
+def get_input(amp, times, t):
+    input = 0
+    if times == "All":
+        input = amp
+    else:
+        for i in range(len(times)):
+            if (i % 2 == 0):
+                if (t > times[i]) and (t < times[i+1]):
+                    input = amp
+    return input
+
+def get_inputs(N, N_times, I, I_times, P, P_times, t):
     inputs = {}
-    inputs["nfkb"] = N
-    inputs["irf"] = I
-    inputs["p50"] = P
+    if N_times == "All":
+        inputs["nfkb"] = N
+    elif N_times == "None":
+        inputs["nfkb"] = 0
+    else:
+        inputs["nfkb"] = get_input(N, N_times, t)
+    if I_times == "All":
+        inputs["irf"] = I
+    elif I_times == "None":
+        inputs["irf"] = 0
+    else:
+        inputs["irf"] = get_input(I, I_times, t)
+    if P_times == "All":
+        inputs["p50"] = P
+    elif P_times == "None":
+        inputs["p50"] = 0
+    else:
+        inputs["p50"] = get_input(P, P_times, t)
     return inputs
 
+   
 def get_params(file):
     params = {}
     with open(file) as f:
@@ -21,29 +48,39 @@ def get_params(file):
             params[key] = float(val)
     return params
 
-def IFN_model(t, states, inputs, params):
+def IFN_model(t, states, params, stim_data):
+    if stim_data is None:
+        N, N_times, I, I_times, P, P_times = 0, "None", 0, "None", 0, "None"
+    else:
+        N, N_times, I, I_times, P, P_times = stim_data
+    # print("N=%s, I=%s, P=%s" % (N, I, P))
+    # print("N_times=%s, I_times=%s, P_times=%s" % (N_times, I_times, P_times))
+    inputs = get_inputs(N, N_times, I, I_times, P, P_times, t)
+    # print("Inputs: " + str(inputs))
+
     ifnb_module = ifnb_change_equations(t, states[0:1], params, inputs)
     inputs["ifnb"] = states[0]
-    ifnar_module = ifnar_change_equations(t, states[2:5], params, inputs)
+    ifnar_module = ifnar_change_equations(t, states[1:5], params, inputs)
     inputs["isgf3"] = states[4]
     isg_module = isg_change_equations(t, states[5:6], params, inputs)
+    return np.concatenate((ifnb_module, ifnar_module, isg_module))
 
-
-def run_model(N, I, P, t_span, states0, params, t_eval=None):
-    inputs = get_inputs(N,I,P)
-    states = solve_ivp(IFN_model, t_span, states0, args=(inputs, params), t_eval=t_eval)
+def run_model(t_span, states0, params, t_eval=None, stim_data=None):
+    states = solve_ivp(IFN_model, t_span, states0, args=(params, stim_data), t_eval=t_eval)
     return states
 
-def get_steady_state(ifnb, t_span, states0, params):
-    states = run_model(ifnb, t_span, states0, params)
+def get_steady_state(t_span, states0, params, stim_data=None):
+    states = run_model(t_span, states0, params, stim_data=stim_data)
     difference = 1
     i = 1
-    while difference < 0.005:
-        states = run_model(ifnb, t_span, states.y[:,-1], params)
-        difference = np.min(np.abs(states.y[:,-1] - states.y[:,-2]))
+    while difference > 0.005:
+        states = run_model(t_span, states.y[:,-1], params, stim_data=stim_data)
+        difference = np.max(np.abs(states.y[0:-2,-1] - states.y[0:-2,-2]))
+        # print("Change in ISG mRNA: %.4f" % (states.y[-1,-1] - states.y[-1,-2]))
         i += 1
         if i > 100:
-            print("No steady state found")
+            max_diff_state = np.argmax(np.abs(states.y[0:-2,-1] - states.y[0:-2,-2]))
+            print("No steady state found; max difference = %.4f, occuring at state #%d" % (difference, max_diff_state))
             break
     final_time = t_span[1]*i
     return states.y[:,-1], final_time
