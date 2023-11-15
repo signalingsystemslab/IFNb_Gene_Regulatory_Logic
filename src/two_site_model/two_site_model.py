71b589eef8bf8a8f58e67e938d716e13ab140752
aschiffman <schiffman@ucla.edu>
2023-11-15 10:31:56 -0800

    Two site model optimizing t-pars

--- src/two_site_model/two_site_model.py
+++ src/two_site_model/two_site_model.py
@@ -2,12 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class two_site:
-    def __init__(self, C, model):
+    def __init__(self, C, model, t=None):
         t_pars = {"IRF":[0,1,0,1],
                   "NFkB":[0,0,1,1],
                   "AND":[0,0,0,1],
                   "OR":[0,1,1,1],}
-        self.t = t_pars[model]
+        if model in t_pars.keys():
+            self.t = t_pars[model]
+        else:
+            self.t = t
         self.beta = np.array([1 for i in range(4)])
         self.beta[3] = C
     def calculateState(self, N, I):
@@ -23,8 +26,8 @@ def explore_two_site(C, model, N, I):
     m.calculateF()
     return m.f
 
-def get_f(C, N, I, model_name):
-    m = two_site(C, model_name)
+def get_f(C, N, I, model_name, t=None):
+    m = two_site(C, model_name, t)
     m.calculateState(N, I)
     m.calculateF()
     return m.f
@@ -41,3 +44,12 @@ def plot_contour(f_values, model_name, I, N, dir, name, condition ="", normalize
 	fig.gca().set_xlabel(r"$IRF$")
 	plt.savefig("%s/contour_plot_%s.png" % (dir,name))
 	plt.close()
+     
+def get_f_contribution(C, N, I, model_name, t=None):
+	model = two_site(C, model_name, t)
+	model.calculateState(N, I)
+
+	f_contributions = model.beta * model.t * model.state / np.dot(np.transpose(model.state), model.beta)
+	state_names = ["none", "IRF", r"$NF\kappa B$", "IRF and $NF\kappa B$"]
+	
+	return f_contributions, state_names
