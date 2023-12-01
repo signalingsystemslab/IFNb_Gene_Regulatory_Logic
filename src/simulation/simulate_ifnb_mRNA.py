466770ce9faffcff166d74d124150d5644e36fe3
aschiffman <schiffman@ucla.edu>
2023-11-30 16:15:43 -0800

    more plots

--- src/simulation/simulate_ifnb_mRNA.py
+++ src/simulation/simulate_ifnb_mRNA.py
@@ -1,4 +1,5 @@
 from simulation_functions import *
+from ifnb_model import *
 
 def initialize(t_pars, other_pars, stimulus, genotype, stim_time = 60*8, stim_data=None, plot = True):
     pars = get_t_params(t_pars)
@@ -106,10 +107,10 @@ def main():
         lps_stim_data = lps_results[0][2]
 
         input_t = np.linspace(0, stim_time+1, stim_time+1+1)
-        plot_inputs(input_t, cpg_stim_data[0], cpg_stim_data[1], cpg_stim_data[2], "CpG", dir)
-        plot_inputs(input_t, lps_stim_data[0], lps_stim_data[1], lps_stim_data[2], "LPS", dir)
+        N_curve_lps, I_curve_lps, P_curve_lps = plot_inputs(input_t, cpg_stim_data[0], cpg_stim_data[1], cpg_stim_data[2], "CpG", dir)
+        N_curve_cpg, I_curve_cpg, P_curve_cpg = plot_inputs(input_t, lps_stim_data[0], lps_stim_data[1], lps_stim_data[2], "LPS", dir)
 
-        # Plot inputs unscaled
+        # # Plot inputs unscaled
         # I_curve_cpg = read_inputs("IRF", "CpG", scale=False)
         # N_curve_cpg = read_inputs("NFkB", "CpG", scale=False)
         # I_curve_lps = read_inputs("IRF", "LPS", scale=False)
@@ -135,7 +136,92 @@ def main():
         # ax[1].legend()
         # plt.savefig("%s/nfkb_irf_timecourses.png" % dir)
 
+        # Take 10 timepoints from each timecourse
+        print("Calculating f for 20 timepoints", flush=True)
+        # LPS WT
+        idx = np.round(np.linspace(0, len(N_curve_lps)-1, 20)).astype(int)
+        # print("Timepoints: " + str(idx), flush=True)
+        # print(type(idx[0]), flush=True)
+        N_LPS = np.array(N_curve_lps)[idx]
+        I_LPS = np.array(I_curve_lps)[idx]
+        f_LPS = np.zeros((num_par_reps, len(idx)))
+        with Pool(40) as p:
+            for i in range(len(idx)):
+                irf = I_LPS[i]
+                nfkb = N_LPS[i]
+                f_LPS[:,i] = p.starmap(get_f, [(t_params[j,:], pars["K_i2"], pars["C"], nfkb, irf, 1.0, "B1", True) for j in range(num_par_reps)])
+
+        # CpG WT
+        N_CpG = np.array(N_curve_cpg)[idx]
+        I_CpG = np.array(I_curve_cpg)[idx]
+        f_CpG = np.zeros((num_par_reps, len(idx)))
+        with Pool(40) as p:
+            for i in range(len(idx)):
+                irf = I_CpG[i]
+                nfkb = N_CpG[i]
+                f_CpG[:,i] = p.starmap(get_f, [(t_params[j,:], pars["K_i2"], pars["C"], nfkb, irf, 1.0, "B1", True) for j in range(num_par_reps)])
+        
+        # Plot f vs time
+        t = input_t[idx]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(num_par_reps):
+            ax.plot(t, f_LPS[i,:], color="b", alpha=0.1, label="LPS" if i == 0 else None)
+            ax.plot(t, f_CpG[i,:], color="r", alpha=0.1, label="CpG" if i == 0 else None)
+        ax.set_xlabel("Time (min)")
+        ax.set_ylabel("f")
+        ax.set_title("f vs time WT")
+        ax.set_ylim([0,1])
+        ax.legend()
+        plt.savefig("%s/f_vs_time_cpg_lps.png" % dir)
+        plt.close()
+
+        # LPS p50KO
+        f_LPSp50KO = np.zeros((num_par_reps, len(idx)))
+        with Pool(40) as p:
+            for i in range(len(idx)):
+                irf = I_LPS[i]
+                nfkb = N_LPS[i]
+                f_LPSp50KO[:,i] = p.starmap(get_f, [(t_params[j,:], pars["K_i2"], pars["C"], nfkb, irf, 0.0, "B1", True) for j in range(num_par_reps)])
+
+        # CpG p50KO
+        f_CpGp50KO = np.zeros((num_par_reps, len(idx)))
+        with Pool(40) as p:
+            for i in range(len(idx)):
+                irf = I_CpG[i]
+                nfkb = N_CpG[i]
+                f_CpGp50KO[:,i] = p.starmap(get_f, [(t_params[j,:], pars["K_i2"], pars["C"], nfkb, irf, 0.0, "B1", True) for j in range(num_par_reps)])
+
+        # Plot f vs time
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(num_par_reps):
+            ax.plot(t, f_LPSp50KO[i,:], color="b", alpha=0.1, label="LPS" if i == 0 else None)
+            ax.plot(t, f_CpGp50KO[i,:], color="r", alpha=0.1, label="CpG" if i == 0 else None)
+        ax.set_xlabel("Time (min)")
+        ax.set_ylabel("f")
+        ax.set_title("f vs time p50KO")
+        ax.set_ylim([0,1])
+        ax.legend()
+        plt.savefig("%s/f_vs_time_cpg_lps_p50KO.png" % dir)
+        plt.close()
+
+        # p50 KO/ WT
+        f_LPS_FC = f_LPSp50KO / f_LPS
+        f_CpG_FC = f_CpGp50KO / f_CpG
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(num_par_reps):
+            ax.plot(t, f_LPS_FC[i,:], color="b", alpha=0.1, label="LPS" if i == 0 else None)
+            ax.plot(t, f_CpG_FC[i,:], color="r", alpha=0.1, label="CpG" if i == 0 else None)
+        ax.set_xlabel("Time (min)")
+        ax.set_ylabel("f fold change (p50KO/WT)")
+        ax.set_title("f fold change vs time")
+        ax.legend()
+        plt.savefig("%s/f_vs_time_FC_cpg_lps.png" % dir)
+
         # Load results
+        print("Loading results", flush=True)
         cpg_timecourses = np.loadtxt("%s/cpg_results.csv" % dir, delimiter=",")
         lps_timecourses = np.loadtxt("%s/lps_results.csv" % dir, delimiter=",")
         t_eval = np.loadtxt("%s/t_eval.csv" % dir, delimiter=",")
