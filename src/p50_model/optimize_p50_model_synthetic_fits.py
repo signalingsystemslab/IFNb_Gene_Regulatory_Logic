cb15e5cf45e4a8072fb4bb45271b82c2b55f48fe
aschiffman <schiffman@ucla.edu>
2023-12-05 16:56:25 -0800

    Plot residuals

--- src/p50_model/optimize_p50_model_synthetic_fits.py
+++ src/p50_model/optimize_p50_model_synthetic_fits.py
@@ -28,10 +28,8 @@ def p50_objective(pars, *args):
         K = pars[6]
         C = pars[7]
 
-    f_list = [get_f(t_pars, K, C, N[i], I[i], P[i], model_name) for i in range(num_pts)]
-    # # Normalize to highest value
-    if np.max(f_list) != 0:
-        f_list = f_list / np.max(f_list)
+    f_list = [get_f(t_pars, K, C, N[i], I[i], P[i], model_name, True) for i in range(num_pts)]
+    # f-values are normalized to pIC WT
     residuals = np.array(f_list) - beta
     if strategy != "rmsd":
         return residuals
@@ -178,7 +176,8 @@ def main():
     #     residuals_all[i] = residuals
     #     cost_all[i] = cost
         
-
+    # print("Saving p50 model with grid optimization at %s" % time.ctime())
+    
     # # Save results
     # np.savetxt("%s/p50_all_datasets_cost_local.csv" % results_dir, cost_all, delimiter=",")
     # np.savetxt("%s/p50_all_datasets_residuals_local.csv" % results_dir, residuals_all, delimiter=",")
@@ -191,7 +190,7 @@ def main():
     pars_all = np.loadtxt("%s/p50_all_datasets_pars_local.csv" % results_dir, delimiter=",")
     residuals = np.loadtxt("%s/p50_all_datasets_residuals_local.csv" % results_dir, delimiter=",")
 
-    # Calculate rmsd and aic for each dataset
+        # Calculate rmsd and aic for each dataset
     rmsd = np.sqrt(np.mean(residuals**2, axis=1))
     np.savetxt("%s/p50_all_datasets_rmsd_local.csv" % results_dir, rmsd, delimiter=",")
     aic = num_pts * np.log(rmsd) + (2 * num_pars)
@@ -202,11 +201,13 @@ def main():
     print("###############################################\n", flush=True)
 
     # Plot parameters
-    def jitter_dots(dots, jitter=0.3):
+    def jitter_dots(dots, jitter=0.3, y_jitter=False):
         offsets = dots.get_offsets()
         jittered_offsets = offsets
         # only jitter in the x-direction
         jittered_offsets[:, 0] += np.random.uniform(-jitter, jitter, size=offsets.shape[0])
+        if y_jitter:
+            jittered_offsets[:, 1] += np.random.uniform(-jitter, jitter, size=offsets.shape[0])
         dots.set_offsets(jittered_offsets)
         return dots
 
@@ -230,6 +231,16 @@ def main():
     plt.colorbar(dots, label="RMSD")
     plt.savefig("%s/p50_all_datasets_pars_local.png" % figures_dir)
 
+    # Plot residuals
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+    for i in range(num_datasets):
+        dots = ax.scatter(np.arange(num_pts), residuals[i,:], c="k", s=50, alpha=0.5)
+        dots = jitter_dots(dots)
+    ax.set_xticks(np.arange(num_pts))
+    ax.set_ylabel("Residual")
+    ax.set_xlabel("Data point")
+    ax.set_title("Residuals for each dataset")
+    plt.savefig("%s/p50_all_datasets_residuals_local.png" % figures_dir)
 
     # Plot parameters all black
     fig, ax = plt.subplots(figsize=(8,6), dpi=300)
@@ -308,92 +319,95 @@ def main():
     fig.legend(bbox_to_anchor=(1.1,0.5))
     plt.savefig("%s/p50_all_datasets_contributions_LPS_CpG_WT.png" % figures_dir)
 
+    # Plot f-value for each parameter set in WT
+    N = training_data.loc[(training_data["Dataset"] == datasets[0]) & (training_data["Genotype"] == "WT")]["NFkB"].values
+    P = 1.0
+    num_points_WT = len(N)
+    f_values_WT = np.zeros((num_datasets, num_points_WT))
+    max_f = np.zeros(num_datasets)
+    for i in range(num_datasets):
+        N = training_data.loc[(training_data["Dataset"] == datasets[i]) & (training_data["Genotype"] == "WT")]["NFkB"].values
+        I = training_data.loc[(training_data["Dataset"] == datasets[i]) & (training_data["Genotype"] == "WT")]["IRF"].values
+        f_values_WT[i,:] = [get_f(pars_all[i,:], 1, 1, N[j], I[j], P, model, True) for j in range(num_points_WT)]
+        max_f[i] = np.max(f_values_WT[i,:])
+        f_values_WT[i,:] = f_values_WT[i,:] / max_f[i]
 
-    # # Plot f-value for each parameter set in WT
-    # N = training_data.loc[training_data["Genotype"] == "WT"]["NFkB"].values
-    # I = training_data.loc[training_data["Genotype"] == "WT"]["IRF"].values
-    # P = 1.0
-    # f_values_WT = np.zeros((num_datasets, num_pts))
-    # max_f = np.zeros(num_datasets)
-    # for i in range(num_datasets):
-    #     f_values_WT[i,:] = [get_f(pars_all[i,:], 1, 1, N[j], I[j], P, model) for j in range(num_pts)]
-    #     max_f[i] = np.max(f_values_WT[i,:])
-    #     f_values_WT[i,:] = f_values_WT[i,:] / max_f[i]
-
-    # cmap = plt.cm.viridis
-    # fig, ax = plt.subplots(figsize=(8,6), dpi=300)
-    # for i in range(num_datasets):
-    #     ax.scatter(N, I, c=f_values_WT[i,:], cmap=cmap, s=50, alpha=0.5, vmin=0, vmax=1)
-    # ax.set_xlabel(r"$NF\kappa B$")
-    # ax.set_ylabel(r"$IRF$")
-    # ax.set_title("Model %s f-values for WT dataset" % model)
+    cmap = plt.cm.viridis
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+    for i in range(num_datasets):
+        p = ax.scatter(N, I, c=f_values_WT[i,:], cmap=cmap, s=25, alpha=0.5, vmin=0, vmax=1)
+        p = jitter_dots(p, y_jitter=True, jitter=0.05)
+    ax.set_xlabel(r"$NF\kappa B$")
+    ax.set_ylabel(r"$IRF$")
+    ax.set_title("Model %s f-values for WT dataset" % model)
     # plt.colorbar(label="f-value")
-    # plt.savefig("%s/p50_all_datasets_f_values_WT.png" % figures_dir)
+    plt.colorbar(p, label="f-value")
+    plt.savefig("%s/p50_all_datasets_f_values_WT.png" % figures_dir)
 
-    # # Plot top 10 parameters with lowest rmsd
-    # fig, ax = plt.subplots(figsize=(8,6), dpi=300)
-    # colors = rmsd[1:]
-    # top_10 = np.argsort(colors)[0:10]
-    # for i in range(num_pars):
-    #     dots = ax.scatter(np.full(10, i), pars_all[top_10,i], c=colors[top_10], cmap="viridis", s=50, alpha=0.5,
-    #                         vmin=np.min(colors), vmax=np.max(colors))
-    #     dots = jitter_dots(dots)
-    #     ax.scatter(i, pars_all[0,i], c="k", s=50, alpha=1)
-    # ax.set_xticks(np.arange(num_pars))
-    # ax.set_xticklabels(par_names)
-    # ax.set_xlim([-0.5, num_pars-0.5])
-    # ax.set_ylabel("Optimized parameter (t) value")
-    # ax.set_xlabel("Parameter")
-    # ax.set_title("Optimized parameter values for model %s" % model)
-    # plt.colorbar(dots, label="RMSD")
-    # plt.savefig("%s/p50_all_datasets_pars_local_top_10.png" % figures_dir)
+    # Plot top 10 parameters with lowest rmsd
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+    colors = rmsd[1:]
+    top_10 = np.argsort(colors)[0:10]
+    for i in range(num_pars):
+        dots = ax.scatter(np.full(10, i), pars_all[top_10,i], c=colors[top_10], cmap="viridis", s=50, alpha=0.5,
+                            vmin=np.min(colors), vmax=np.max(colors))
+        dots = jitter_dots(dots)
+        ax.scatter(i, pars_all[0,i], c="k", s=50, alpha=1)
+    ax.set_xticks(np.arange(num_pars))
+    ax.set_xticklabels(par_names)
+    ax.set_xlim([-0.5, num_pars-0.5])
+    ax.set_ylabel("Optimized parameter (t) value")
+    ax.set_xlabel("Parameter")
+    ax.set_title("Optimized parameter values for model %s" % model)
+    plt.colorbar(dots, label="RMSD")
+    plt.savefig("%s/p50_all_datasets_pars_local_top_10.png" % figures_dir)
 
-    # # Plot rmsd for each dataset
-    # fig, ax = plt.subplots(figsize=(8,6), dpi=300)
-    # ax.scatter(np.arange(num_datasets), rmsd, c="k", s=50)
-    # ax.set_ylabel("RMSD")
-    # ax.set_xlabel("Dataset")
-    # ax.set_xticks(np.arange(num_datasets))
-    # ax.set_xticklabels(datasets, rotation=90)
-    # ax.set_title("RMSD for each dataset")
-    # plt.savefig("%s/p50_all_datasets_rmsd_local.png" % figures_dir)
+    # Plot rmsd for each dataset
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+    ax.scatter(np.arange(num_datasets), rmsd, c="k", s=50)
+    ax.set_ylabel("RMSD")
+    ax.set_xlabel("Dataset")
+    ax.set_xticks(np.arange(num_datasets))
+    ax.set_xticklabels(datasets, rotation=90)
+    ax.set_title("RMSD for each dataset")
+    plt.savefig("%s/p50_all_datasets_rmsd_local.png" % figures_dir)
 
-    # # Plot each parameter distribution as density plot
-    # fig, ax = plt.subplots(2,3, figsize=(12,8), dpi=300)
-    # ax = ax.flatten()
-    # for i in range(num_pars):
-    #     ax[i].hist(pars_all[:,i], bins=20, density=True, color="k", alpha=0.5)
-    #     ax[i].set_title(par_names[i])
-    #     ax[i].set_xlabel("Optimized parameter (t) value")
-    #     ax[i].set_ylabel("Density")
-    # plt.tight_layout()
-    # plt.suptitle("Parameter distributions for model %s" % model)
-    # plt.savefig("%s/p50_all_datasets_pars_local_hist.png" % figures_dir) 
+    # Plot each parameter distribution as density plot
+    fig, ax = plt.subplots(2,3, figsize=(12,8), dpi=300)
+    ax = ax.flatten()
+    for i in range(num_pars):
+        ax[i].hist(pars_all[:,i], bins=20, density=True, color="k", alpha=0.5)
+        ax[i].set_title(par_names[i])
+        ax[i].set_xlabel("Optimized parameter (t) value")
+        ax[i].set_ylabel("Density")
+    plt.tight_layout()
+    plt.suptitle("Parameter distributions for model %s" % model)
+    plt.savefig("%s/p50_all_datasets_pars_local_hist.png" % figures_dir) 
 
-    # # Plot joint distribution of parameters
-    # fig, ax = plt.subplots(num_pars, num_pars, figsize=(12,12), dpi=300)
-    # for i in range(num_pars):
-    #     for j in range(num_pars):
-    #         ax[i,j].scatter(pars_all[:,i], pars_all[:,j], c="k", s=50, alpha=0.5)
-    #         ax[i,j].set_xlabel(par_names[i])
-    #         ax[i,j].set_ylabel(par_names[j])
-    # plt.tight_layout()
-    # plt.savefig("%s/p50_all_datasets_pars_local_joint.png" % figures_dir)
+    # Plot joint distribution of parameters
+    fig, ax = plt.subplots(num_pars, num_pars, figsize=(12,12), dpi=300)
+    for i in range(num_pars):
+        for j in range(num_pars):
+            ax[i,j].scatter(pars_all[:,i], pars_all[:,j], c="k", s=50, alpha=0.5)
+            ax[i,j].set_xlabel(par_names[i])
+            ax[i,j].set_ylabel(par_names[j])
+    plt.tight_layout()
+    plt.savefig("%s/p50_all_datasets_pars_local_joint.png" % figures_dir)
 
-    # # Make contour plots from top 10 parameters
-    # top_pars = pars_all[top_10, :]
-    # for i in range(10):
-    #     t_pars = top_pars[i]
-    #     K = 1
-    #     C = 1
-    #     N = np.linspace(0, 1, 50)
-    #     I = np.linspace(0, 1, 50)
-    #     P = np.ones(50)
-    #     f_values = np.zeros((len(N), len(I)))
-    #     for j in range(len(N)):
-    #         for k in range(len(I)):
-    #             f_values[j,k] = get_f(t_pars, K, C, N[j], I[k], P[k], model)
-    #     plot_contour(f_values, model, I, N, figures_dir, "top_10_%d" % i, condition="top 10 #%d" % i, normalize=True)
+    # Make contour plots from top 10 parameters
+    top_pars = pars_all[top_10, :]
+    for i in range(10):
+        t_pars = top_pars[i]
+        K = 1
+        C = 1
+        N = np.linspace(0, 1, 50)
+        I = np.linspace(0, 1, 50)
+        P = np.ones(50)
+        f_values = np.zeros((len(N), len(I)))
+        for j in range(len(N)):
+            for k in range(len(I)):
+                f_values[j,k] = get_f(t_pars, K, C, N[j], I[k], P[k], model, True)
+        plot_contour(f_values, model, I, N, figures_dir, "top_10_%d" % i, condition="top 10 #%d" % i, normalize=True)
 
 
 
