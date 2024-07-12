5529bd746684385fa34470378a0927fef53da5d8
aschiffman <schiffman@ucla.edu>
2024-07-12 16:21:47 -0700

    Added optional c parameter

--- src/p50_model/optimize_p50_model.py
+++ src/p50_model/optimize_p50_model.py
@@ -2,7 +2,7 @@
 # Initial parameters from parameter scan
 
 from p50_model import *
-from parameter_scan_p50_model import calc_state_prob, plot_state_probabilities, plot_parameter_distributions, get_N_I_P
+from parameter_scan_p50_model import plot_state_probabilities, plot_parameter_distributions, get_N_I_P
 from p50_model_figures import plot_parameters, plot_predictions
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,6 +13,11 @@ from multiprocessing import Pool
 import argparse
 import seaborn as sns
 # import scipy.stats.qmc as qmc
+
+def calc_state_prob(k_pars, N, I, P, h_pars, num_t):
+    t_pars = [1 for _ in range(num_t)]
+    probabilities, state_names = get_state_prob(t_pars, k_pars, N, I, P, h_pars=h_pars)
+    return probabilities, state_names
 
 def calculate_ifnb(pars, data, h_pars, c=None, num_t_pars=5, num_k_pars=4):
     t_pars, k_pars = pars[:num_t_pars], pars[num_t_pars:]
@@ -25,21 +30,32 @@ def p50_objective(pars, *args):
     """Minimization objective function for optimizing a k parameter.
     Args:
     pars: array of parameters
-    args: tuple of (N, I, IFNb_data)
-    kwargs: additional parameters (c, h)
+    args: 
     """
 
-    N, I, P, beta, h_pars, which_pars, other_pars = args
+    N, I, P, beta, h_pars, c_par, which_pars, other_pars = args
 
     if which_pars == "All":
+        if c_par is None:
+            if len(pars) != len(other_pars):
+                raise ValueError("Number of parameters (%d) does not match number of t and k parameters (%d)" % (len(pars), len(other_pars)))
+        else:
+            if len(pars) != len(other_pars) + 1:
+                raise ValueError("Number of parameters (%d + c) does not match number of t and k parameters (%d)" % (len(pars) -1, len(other_pars)))
+    elif which_pars == "alltk":
         if len(pars) != len(other_pars):
-            raise ValueError("Number of parameters (%d) does not match number of t and k parameters (%d)" % (len(pars), other_pars.shape[1]))
+            raise ValueError("Number of parameters (%d) does not match number of t and k parameters (%d)" % (len(pars), len(other_pars)))
     elif len(which_pars) != len(pars):
         raise ValueError("Number of parameters (%d) does not match par names (%d)" % (len(pars), len(which_pars)))
 
     t_pars = other_pars[0:5]
     k_pars = other_pars[5:9]
     if which_pars == "All":
+        t_pars = pars[0:5]
+        k_pars = pars[5:9]
+        if c_par is not None:
+            c_par = pars[9]
+    elif which_pars == "alltk":
         t_pars = pars[0:5]
         k_pars = pars[5:9]
     else:
@@ -57,21 +73,23 @@ def p50_objective(pars, *args):
                 h_pars[0] = pars[i]
             elif par == "h2":
                 h_pars[1] = pars[i]
+            elif par == "c":
+                c_par = pars[i]
             else:
                 raise ValueError("Parameter %s not recognized" % par)
         
     num_pts = len(N)
     
-    f_list = [get_f(t_pars, k_pars, N[i], I[i], P[i], h_pars=h_pars) for i in range(num_pts)] 
+    f_list = [get_f(t_pars, k_pars, N[i], I[i], P[i], c_par=c_par, h_pars=h_pars) for i in range(num_pts)] 
     residuals = np.array(f_list) - beta
     
     rmsd = np.sqrt(np.mean(residuals**2))
     return rmsd
 
-def minimize_objective(pars, N, I, P, beta, h_pars, which_pars, other_pars, bounds):
-    return opt.minimize(p50_objective, pars, args=(N, I, P, beta, h_pars, which_pars, other_pars), method="Nelder-Mead", bounds=bounds)
+def minimize_objective(pars, N, I, P, beta, h_pars, c_par, which_pars, other_pars, bounds):
+    return opt.minimize(p50_objective, pars, args=(N, I, P, beta, h_pars, c_par, which_pars, other_pars), method="Nelder-Mead", bounds=bounds)
 
-def optimize_model(N, I, P, beta, initial_pars, h, opt_pars, num_threads=40):
+def optimize_model(N, I, P, beta, initial_pars, h, c, opt_pars, num_threads=40):
     '''
     Optimize the parameters of the model
     N: list or array of NFkB concentrations
@@ -91,6 +109,8 @@ def optimize_model(N, I, P, beta, initial_pars, h, opt_pars, num_threads=40):
     max_k_order = 4
     min_h = 1
     max_h = 5
+    min_c = 10**-2
+    max_c = 10**2
 
     pars_locs = {"t1":0, "t2":1, "t3":2, "t4":3, "t5":4, "k1":5, "k2":6, "kn":7, "kp":8}
     h_locs = {"h1":0, "h2":1}
@@ -100,8 +120,13 @@ def optimize_model(N, I, P, beta, initial_pars, h, opt_pars, num_threads=40):
     if opt_pars == "All":
         bnds = [(0,1) for i in range(5)]
         bnds += [(10**min_k_order, 10**max_k_order) for i in range(4)]
+        if c is not None:
+            bnds.append((min_c, max_c))
+            pars = np.concatenate((initial_pars, np.repeat(c, len(initial_pars)).reshape(-1,1)), axis=1)
+    elif opt_pars == "alltk":
+        bnds = [(0,1) for i in range(5)]
+        bnds += [(10**min_k_order, 10**max_k_order) for i in range(4)]
         pars = initial_pars
-
     else:
         pars=np.zeros((len(initial_pars), len(opt_pars)))
         for i in range(len(opt_pars)):
@@ -112,17 +137,20 @@ def optimize_model(N, I, P, beta, initial_pars, h, opt_pars, num_threads=40):
             elif par[0] == "h":
                 bnds.append((min_h, max_h))
                 pars[:,i] = h[:,h_locs[par]]
+            elif par[0] == "c":
+                bnds.append((min_c, max_c))
+                pars[:,i] = c
             else:
                 raise ValueError("Parameter %s not recognized" % par)
         # print(pars)
     # Optimize
     with Pool(num_threads) as p:
-        results = p.starmap(minimize_objective, [(pars[i], N, I, P, beta, h, opt_pars, initial_pars[i], bnds) for i in range(len(pars))])
+        results = p.starmap(minimize_objective, [(pars[i], N, I, P, beta, h, c, opt_pars, initial_pars[i], bnds) for i in range(len(pars))])
     
     final_pars = np.array([result.x for result in results]) # each row is a set of optimized parameters
     rmsd = np.array([result.fun for result in results])
 
-    if opt_pars != "All":
+    if opt_pars not in ["All", "alltk"]:
         final_pars_temp = initial_pars.copy()
         # replace the optimized parameters with the initial parameters for the parameters that were not optimized
         for i in range(len(opt_pars)):
@@ -154,7 +182,8 @@ def optimize_model(N, I, P, beta, initial_pars, h, opt_pars, num_threads=40):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o","--optimize", action="store_true")
-    parser.add_argument("-p","--optimize_kp", action="store_true")
+    parser.add_argument("-k","--optimize_kp", action="store_true")
+    parser.add_argument("-c","--optimize_c", action="store_true")
     parser.add_argument("-s","--state_probabilities", action="store_true")
     args = parser.parse_args()
 
@@ -169,19 +198,27 @@ def main():
     par_type = "k"
     h_val = "3_3_1"
     h1, h2, h3 = [int(h) for h in h_val.split("_")]
+    c = 3
+    opt_c = args.optimize_c
 
     # Model details
     num_t_pars = 5
     num_k_pars = 4
-    num_c_pars = 0
-   
-    num_pars = num_t_pars + num_k_pars
+    num_c_pars = 1
+    num_h_pars = 2
 
     # Directories
     figures_dir = "optimization/figures/"
     results_dir = "optimization/results/"
+    if c is not None:
+        figures_dir = figures_dir[:-1] + "_c_%d/" % c
+        results_dir = results_dir[:-1] + "_c_%d/" % c
+        if opt_c:
+            figures_dir = figures_dir[:-1] + "_opt_c/"
+            results_dir = results_dir[:-1] + "_opt_c/"
     os.makedirs(figures_dir, exist_ok=True)
     os.makedirs(results_dir, exist_ok=True)
+    print("Saving results to %s" % results_dir, flush=True)
     pars_dir = "../three_site_model/optimization/results/seed_0/"
     
     # Load training data
@@ -201,12 +238,15 @@ def main():
     initial_pars = np.loadtxt("%s/three_site_only_hill_optimized_parameters_h_%s.csv" % (pars_dir, h_val), delimiter=",")
     initial_pars = np.concatenate((initial_pars, np.ones((len(initial_pars),1))), axis=1) # Add kp parameter
     initial_pars_names = ["t%d" % (i+1) for i in range(num_t_pars)] + ["k%d" % (i+1) for i in range(num_k_pars)]
-    result_par_names = initial_pars_names + ["h1", "h2"]
+    if c is None or not opt_c:
+        result_par_names = initial_pars_names + ["h1", "h2"]
+    else:
+        result_par_names = initial_pars_names + ["c", "h1", "h2"]
 
     # Calculate rmsd for initial parameters
     beta_stacked = np.stack([beta for _ in range(len(initial_pars))], axis=0)
     with Pool(num_threads) as p:
-        results = p.starmap(calculate_ifnb, [(pars, training_data, [h1, h2]) for pars in initial_pars])
+        results = p.starmap(calculate_ifnb, [(pars, training_data, [h1, h2], c) for pars in initial_pars])
 
     ifnb_predicted = np.array(results)
     residuals = ifnb_predicted - beta_stacked
@@ -216,8 +256,11 @@ def main():
     if args.optimize:
         # Optimize all k and t parameters
         print("Optimizing all k and t parameters...", flush=True)
-        par_names = "All"
-        final_pars, rmsd = optimize_model(N, I, P, beta, initial_pars, [h1, h2], par_names, num_threads=num_threads)
+        if opt_c:
+            par_names = "All"
+        else:
+            par_names="alltk"
+        final_pars, rmsd = optimize_model(N, I, P, beta, initial_pars, [h1, h2], c, par_names, num_threads=num_threads)
         np.savetxt("%s/%s_optimized_parameters.csv" % (results_dir, model), final_pars, delimiter=",")
         np.savetxt("%s/%s_rmsd.csv" % (results_dir, model), rmsd, delimiter=",")
     elif not args.optimize_kp:
@@ -237,7 +280,7 @@ def main():
         # Plot state probabilities
         if args.state_probabilities:
             print("Calculating and plotting state probabilities...", flush=True)
-            all_k_pars = final_pars[:, num_t_pars:]
+            all_k_pars = final_pars[:, num_t_pars:-num_h_pars]
 
             extra_training_data = pd.DataFrame({"Stimulus":"basal", "Genotype":"WT", "IRF":0.01, "NFkB":0.01, "p50":1}, index=[0])
             training_data_extended = pd.concat([training_data, extra_training_data], ignore_index=True)
@@ -251,7 +294,7 @@ def main():
                         print("N=%.2f, I=%.2f, P=%.2f" % (nfkb, irf, p50), flush=True)
                     
                         with Pool(num_threads) as p:
-                            results = p.starmap(calc_state_prob, [(tuple(all_k_pars[i]), nfkb, irf, p50) for i in range(len(all_k_pars))])
+                            results = p.starmap(calc_state_prob, [(tuple(all_k_pars[i]), nfkb, irf, p50, [h1, h2], num_t_pars) for i in range(len(all_k_pars))])
                     
                         state_names = results[0][1]
                         state_probabilities = np.array([x[0] for x in results])
@@ -297,7 +340,7 @@ def main():
         # Optimize kp
         print("Optimizing parameter kp...", flush=True)
         par_names = ["kp"]
-        final_pars, rmsd = optimize_model(N, I, P, beta, initial_pars, [h1, h2], par_names, num_threads=num_threads)
+        final_pars, rmsd = optimize_model(N, I, P, beta, initial_pars, [h1, h2], c, par_names, num_threads=num_threads)
         np.savetxt("%s/%s_optimized_parameters_kp.csv" % (results_dir, model), final_pars, delimiter=",")
         np.savetxt("%s/%s_rmsd_kp.csv" % (results_dir, model), rmsd, delimiter=",")
 
