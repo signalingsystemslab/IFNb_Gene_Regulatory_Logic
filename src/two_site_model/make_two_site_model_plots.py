089c993113ee2375bfa2ca936318433179804ba7
aschiffman <schiffman@ucla.edu>
2025-04-16 14:07:57 -0700

    Cleaned up plotting code

--- src/two_site_model/make_two_site_model_plots.py
+++ src/two_site_model/make_two_site_model_plots.py
@@ -290,181 +290,6 @@ def plot_predictions_one_plot(ifnb_predicted_1, h1, ifnb_predicted_2, h2, ifnb_p
     df_all["Hill"] = pd.Categorical(df_all["Hill"], categories=hill_categories, ordered=True)
     make_predictions_plot(df_all, name, figures_dir)
 
-    
-
-# def plot_predictions_barplots_onefig(ifnb_predicted_1, h1, ifnb_predicted_2, h2, ifnb_predicted_3, h3, ifnb_predicted_4, h4, beta, conditions, name, figures_dir):
-#     df_ifnb_predicted_1 = make_predictions_data_frame(ifnb_predicted_1, beta, conditions)
-#     df_ifnb_predicted_2 = make_predictions_data_frame(ifnb_predicted_2, beta, conditions)
-#     df_ifnb_predicted_3 = make_predictions_data_frame(ifnb_predicted_3, beta, conditions)
-#     df_ifnb_predicted_4 = make_predictions_data_frame(ifnb_predicted_4, beta, conditions)
-
-#     data_df = df_ifnb_predicted_1.loc[df_ifnb_predicted_1["par_set"] == "Data"].copy()
-
-#     df_sym = pd.concat([df_ifnb_predicted_1, 
-#                         df_ifnb_predicted_2,
-#                         df_ifnb_predicted_3,
-#                         df_ifnb_predicted_4], ignore_index=True)
-#     df_sym = pd.concat([df_ifnb_predicted_1, df_ifnb_predicted_2, df_ifnb_predicted_3, df_ifnb_predicted_4], ignore_index=True)
-#     df_sym[r"H_I"] = np.concatenate([np.repeat(h1, len(df_ifnb_predicted_1)), np.repeat(h2, len(df_ifnb_predicted_2)),
-#                                         np.repeat(h3, len(df_ifnb_predicted_3)), np.repeat(h4, len(df_ifnb_predicted_4))])
-#     df_sym["Hill"] = r"$h_{I}$=" + df_sym[r"H_I"]
-    
-#     # data_df[r"H_{I_2}"] = np.repeat("Data", len(data_df))
-#     # data_df[r"H_{I_1}"] = np.repeat("", len(data_df))
-#     data_df[r"H_I"] = np.repeat("Data", len(data_df))
-#     data_df["Hill"] = "Exp."
-
-#     df_sym = df_sym.loc[df_sym["par_set"] != "Data"] # contains duplicate data points
-#     df_all = pd.concat([df_sym, data_df], ignore_index=True)
-
-#     hill_categories = np.concatenate([data_df["Hill"].unique(), df_sym["Hill"].unique()])
-
-#     df_all["Hill"] = pd.Categorical(df_all["Hill"], categories=hill_categories, ordered=True)
-
-#     # print(df_all)
-#     # return 1
-
-#     data = data_df.copy()
-#     barwidth = 0.2 # inch per bar
-#     spacing = 3    # spacing between subplots in units of barwidth
-#     figx = 5       # figure width in inch
-#     left = 4       # left margin in units of bar width
-#     right=2      # right margin in units of bar width
-
-#     tc = len(data["Category"].unique())  # total number of categories
-#     max_values = []  # holds the maximum number of bars to create
-#     for category in data["Category"].unique():
-#         num_bars = len(data[data["Category"]==category]["Data point"].unique())
-#         max_values.append(num_bars)
-#     max_values = np.array(max_values)
-
-#     # print(max_values)
-#     # return 1
-
-#     # total figure height:
-#     figy = ((np.sum(max_values)+tc) + (tc+1)*spacing)*barwidth #inch
-
-#     fig = plt.figure(figsize=(figx,figy))
-#     ax = None
-#     for index, category in enumerate(data["Category"].unique()):
-#         # print(category)
-#         entries = []
-#         values = []
-#         for entry in data[data["Category"]==category]["Data point"].unique():
-#             entries.append(entry)
-#             values.append(data[(data["Category"]==category) & (data["Data point"]==entry)][r"IFN$\beta$"].values[0])
-#         if not entries:
-#             continue  # do not create empty charts
-
-#         # print(entries)
-#         # print(values)
-#         y_ticks = range(1, len(entries) + 1)
-#         # coordinates of new axes [left, bottom, width, height]
-#         coord = [left*barwidth/figx, 
-#                  1-barwidth*((index+1)*spacing+np.sum(max_values[:index+1])+index+1)/figy,  
-#                  1-(left+right)*barwidth/figx,  
-#                  (max_values[index]+1)*barwidth/figy ] 
-
-#         ax = fig.add_axes(coord, sharex=ax)
-#         ax.barh(y_ticks, values)
-#         ax.set_ylim(0, max_values[index] + 1)  # limit the y axis for fixed height
-#         ax.set_yticks(y_ticks)
-#         ax.set_yticklabels(entries)
-#         ax.invert_yaxis()
-#         ax.set_title(category)
-#         plt.savefig("%s/%s.png" % (figures_dir, name), bbox_inches="tight")
-    
-
-# def plot_predictions_one_plot(ifnb_predicted_1, h1, ifnb_predicted_2, h2, ifnb_predicted_3, h3, ifnb_predicted_4, h4, beta, conditions, name, figures_dir):
-#     # Plot predictions for all conditions in one plot. Average of best 20 models for each hill combination with error bars.
-#     df_ifnb_predicted_1 = make_predictions_data_frame(ifnb_predicted_1, beta, conditions)
-#     df_ifnb_predicted_2 = make_predictions_data_frame(ifnb_predicted_2, beta, conditions)
-#     df_ifnb_predicted_3 = make_predictions_data_frame(ifnb_predicted_3, beta, conditions)
-#     df_ifnb_predicted_4 = make_predictions_data_frame(ifnb_predicted_4, beta, conditions)
-
-#     data_df = df_ifnb_predicted_1.loc[df_ifnb_predicted_1["par_set"] == "Data"].copy()
-
-#     df_all = pd.concat([df_ifnb_predicted_1, df_ifnb_predicted_2, df_ifnb_predicted_3, df_ifnb_predicted_4], ignore_index=True)
-#     # df_all[r"H_{I_2}"] = np.concatenate([np.repeat("1", len(df_ifnb_predicted_1)), np.repeat("1", len(df_ifnb_predicted_2)),
-#     #                                     np.repeat("3", len(df_ifnb_predicted_3)), np.repeat("3", len(df_ifnb_predicted_4))])
-#     # df_all[r"H_{I_1}"] = np.concatenate([np.repeat("1", len(df_ifnb_predicted_1)), np.repeat("3", len(df_ifnb_predicted_2)),
-#     #                                     np.repeat("1", len(df_ifnb_predicted_3)), np.repeat("3", len(df_ifnb_predicted_4))])
-#     # df_all["Hill"] = "Model Fit\n" + r"($h_{I_1}$=" + df_all[r"H_{I_1}"] + r", $h_{I_2}$=" + df_all[r"H_{I_2}"] + r")
-#     df_all[r"H_I"] = np.concatenate([np.repeat(h1, len(df_ifnb_predicted_1)), np.repeat(h2, len(df_ifnb_predicted_2)),
-#                                         np.repeat(h3, len(df_ifnb_predicted_3)), np.repeat(h4, len(df_ifnb_predicted_4))])
-#     df_all["Hill"] = "Model Fit\n" + r"($h_{I}$=" + df_all[r"H_I"] + r")"
-    
-#     # data_df[r"H_{I_2}"] = np.repeat("Data", len(data_df))
-#     # data_df[r"H_{I_1}"] = np.repeat("", len(data_df))
-#     data_df[r"H_I"] = np.repeat("Data", len(data_df))
-#     data_df["Hill"] = "Experimental"
-
-#     df_all = df_all.loc[df_all["par_set"] != "Data"] # contains duplicate data points
-#     df_all = pd.concat([df_all, data_df], ignore_index=True)
-    
-
-#     with sns.plotting_context("paper",rc=plot_rc_pars):
-#         colors = models_colors
-#         col = data_color
-#         fig, ax = plt.subplots(figsize=(2.2,2))
-#         # sns.lineplot(data=df_all.loc[df_all["par_set"] != "Data"], x="Data point", y=r"IFN$\beta$", hue="Hill", palette=colors, 
-#         #              ax=ax, err_style="band", errorbar=("pi",50), zorder = 0)
-#         # sns.scatterplot(data=df_all.loc[df_all["par_set"] != "Data"], x="Data point", y=r"IFN$\beta$", hue="Hill", palette=colors, marker="o", ax=ax, 
-#         #                 legend=False, linewidth=0,  zorder = 1)
-        
-#         df_sub = df_all.loc[df_all["par_set"] != "Data"]
-#         unique_hills = np.unique(df_sub["Hill"])
-
-#         for i, hill in enumerate(unique_hills):
-#             # Filter data for the current Hill
-#             df_hill = df_all[df_all["Hill"] == hill]
-
-#             # Create lineplot and scatterplot for the current Hill
-#             sns.lineplot(data=df_hill.loc[df_hill["par_set"] != "Data"], x="Data point", y=r"IFN$\beta$", color=colors[i], 
-#                         ax=ax, err_style="band", errorbar=("pi",50), zorder = i, label=hill)
-#             sns.scatterplot(data=df_hill.loc[df_hill["par_set"] != "Data"], x="Data point", y=r"IFN$\beta$", color=colors[i],
-#                         marker="o", ax=ax, linewidth=0,  zorder = i+0.5)
-
-
-#         sns.lineplot(data=df_all.loc[df_all["par_set"] == "Data"], x="Data point", y=r"IFN$\beta$", color=col, ax=ax, label="Experimental", zorder = 10)
-#         sns.scatterplot(data=df_all.loc[df_all["par_set"] == "Data"], x="Data point", y=r"IFN$\beta$", color=col, marker="o", ax=ax, legend=False, linewidth=0,
-#                          zorder = 11)
-#         xticks = ax.get_xticks()
-#         # for testing
-#         # labels = [item.get_text().replace(" ", "\n") for item in ax.get_xticklabels()]
-        
-#         # ax.set_xticklabels(labels)
-
-#         labels_genotype_only = [item.get_text().split(" ")[1] for item in ax.get_xticklabels()]
-#         # ax.set_xticklabels(labels_genotype_only) # for testing
-#         labels_stimulus_only = [item.get_text().split(" ")[0] for item in ax.get_xticklabels()]
-#         unique_stimuli = np.unique(labels_stimulus_only)
-#         stimuli_locations = {stimulus: np.where(np.array(labels_stimulus_only) == stimulus)[0] for stimulus in unique_stimuli}
-#         stimuli_mean_locs = [np.mean(locations) for stimulus, locations in stimuli_locations.items()]
-#         stimuli_mean_locs = [loc + 10**-5 for loc in stimuli_mean_locs]
-#         xticks = xticks + stimuli_mean_locs
-#         unique_stimuli = ["\n\n\n\n\n\n%s" % stimulus for stimulus in unique_stimuli]
-#         labels = labels_genotype_only + unique_stimuli
-#         ax.set_xticks(xticks)
-#         ax.set_xticklabels(labels)
-
-#         for label in ax.get_xticklabels():
-#             if label.get_text() in labels_genotype_only:
-#                 label.set_rotation(90)
-
-#         # Get all xticks
-#         xticks = ax.xaxis.get_major_ticks()
-
-#         # Remove the tick lines for the last three xticks
-#         for tick in xticks[len(labels_genotype_only):]:
-#             tick.tick1line.set_visible(False)
-#             tick.tick2line.set_visible(False)
-
-#         sns.despine()
-#         plt.tight_layout()
-#         sns.move_legend(ax, bbox_to_anchor=(0.5,1), title=None, frameon=False, loc="lower center", ncol=3)
-#         plt.savefig("%s/%s.png" % (figures_dir, name), bbox_inches="tight")
-#         plt.close()
 
 def plot_predictions_one_plot_with_data(ifnb_predicted_1, h1, ifnb_predicted_2, h2, ifnb_predicted_3, h3, ifnb_predicted_4, h4, 
                                         training_data, name, figures_dir, data_only=False):
@@ -480,17 +305,11 @@ def plot_predictions_one_plot_with_data(ifnb_predicted_1, h1, ifnb_predicted_2, 
     data_df = df_ifnb_predicted_1.loc[df_ifnb_predicted_1["par_set"] == "Data"].copy()
 
     df_all = pd.concat([df_ifnb_predicted_1, df_ifnb_predicted_2, df_ifnb_predicted_3, df_ifnb_predicted_4], ignore_index=True)
-    # df_all[r"H_{I_2}"] = np.concatenate([np.repeat("1", len(df_ifnb_predicted_1)), np.repeat("1", len(df_ifnb_predicted_2)),
-    #                                     np.repeat("3", len(df_ifnb_predicted_3)), np.repeat("3", len(df_ifnb_predicted_4))])
-    # df_all[r"H_{I_1}"] = np.concatenate([np.repeat("1", len(df_ifnb_predicted_1)), np.repeat("3", len(df_ifnb_predicted_2)),
-    #                                     np.repeat("1", len(df_ifnb_predicted_3)), np.repeat("3", len(df_ifnb_predicted_4))])
-    # df_all["Hill"] = "Model Fit\n" + r"($h_{I_1}$=" + df_all[r"H_{I_1}"] + r", $h_{I_2}$=" + df_all[r"H_{I_2}"] + r")
+    
     df_all[r"H_I"] = np.concatenate([np.repeat(h1, len(df_ifnb_predicted_1)), np.repeat(h2, len(df_ifnb_predicted_2)),
                                         np.repeat(h3, len(df_ifnb_predicted_3)), np.repeat(h4, len(df_ifnb_predicted_4))])
     df_all["Hill"] =r"$h_{I}$=" + df_all[r"H_I"]
     
-    # data_df[r"H_{I_2}"] = np.repeat("Data", len(data_df))
-    # data_df[r"H_{I_1}"] = np.repeat("", len(data_df))
     data_df[r"H_I"] = np.repeat("Data", len(data_df))
     data_df["Hill"] = "Experimental"
 
@@ -609,10 +428,7 @@ def make_parameters_data_frame(pars):
     # df_k_pars = df_pars[df_pars["Parameter"].str.startswith("k")]
     df_k_pars = df_pars.loc[df_pars["Parameter"].str.startswith("k") | df_pars["Parameter"].str.startswith("c")].copy()
     num_k_pars = len(df_k_pars["Parameter"].unique())
-    # df_k_pars["Parameter"] = df_k_pars["Parameter"].str.replace("k3", r"$k_N$")
-    # df_k_pars["Parameter"] = df_k_pars["Parameter"].str.replace("k2", r"$k_2$")
-    # df_k_pars["Parameter"] = df_k_pars["Parameter"].str.replace("k1", r"$k_1$")
-    # df_k_pars["Parameter"] = df_k_pars["Parameter"].str.replace("kn", r"$k_N$")
+    
     df_k_pars.loc[df_k_pars["Parameter"] == "k1", "Parameter"] = r"$k_I$" # Rename
     df_k_pars.loc[df_k_pars["Parameter"] == "k2", "Parameter"] = r"$K_N$" # Rename
     df_k_pars.loc[df_k_pars["Parameter"] == "c", "Parameter"] = r"$C$"
@@ -764,56 +580,6 @@ def make_pars_plots(num_t_pars, num_k_pars, df_all_t_pars, df_all_k_pars, name, 
 
         plt.savefig("%s/%s.png" % (figures_dir, name), bbox_inches="tight")
         plt.close()
-    
-    
-    
-    # with sns.plotting_context("paper",rc=plot_rc_pars):
-    #     colors = models_colors
-    #     width = 2.1
-    #     height = 1
-    #     fig, ax = plt.subplots(1,2, figsize=(width, height), 
-    #                            gridspec_kw={"width_ratios":[num_t_pars, num_k_pars]})
-        
-    #     k_parameters = [r"$K_N$"] # Only plot K_N
-
-    #     unique_models = np.unique(df_all_t_pars["Model"])
-    #     legend_handles = []
-    #     for i, model in enumerate(unique_models):
-    #         # Filter data for the current model
-    #         df_model = df_all_t_pars[df_all_t_pars["Model"] == model]
-    #         l = sns.lineplot(data=df_model, x="Parameter", y="Value", color=colors[i], ax=ax[0], zorder = i, errorbar=None, estimator=None, alpha=0.2, units="par_set")
-    #         sns.scatterplot(data=df_model, x="Parameter", y="Value", color=colors[i], ax=ax[0], legend=False, zorder = i+0.5, linewidth=0)
-
-    #         legend_handles.append(l.lines[0])
-
-    #         df_model = df_all_k_pars[(df_all_k_pars["Model"] == model) & (df_all_k_pars["Parameter"].isin(k_parameters))] 
-    #         sns.lineplot(data=df_model, x="Parameter", y="Value", color=colors[i], ax=ax[1], zorder = i, errorbar=None, estimator=None, alpha=0.2, units="par_set")
-    #         sns.scatterplot(data=df_model, x="Parameter", y="Value", color=colors[i], ax=ax[1], legend=False, zorder = i+0.5, linewidth=0, alpha=0.2)
-        
-    #     ax[1].set_yscale("log")
-    #     ax[1].set_ylabel("")
-
-    #     ax[0].set_ylabel("Parameter Value")
-    #     ax[0].set_xlabel("")
-    #     ax[1].set_xlabel("")
-
-    #     sns.despine()
-    #     plt.tight_layout()
-    #     # handles, labels = ax[0].get_legend_handles_labels()
-
-    #     # leg = fig.legend(legend_handles, unique_models, loc='center left', bbox_to_anchor=(1, 0.5), title="Model", frameon=False)
-    #     leg = fig.legend(legend_handles, unique_models, loc='center left', bbox_to_anchor=(1, 0.5), frameon=False,
-    #                     columnspacing=1, handletextpad=0.5, handlelength=1.5)
-
-    #     for i in range(len(unique_models)):
-    #         leg.legend_handles[i].set_alpha(1)
-    #         leg.legend_handles[i].set_color(colors[i])
-
-
-    #     # fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 1), ncol=2, frameon=False)
-    #     # ax[0].get_legend().remove()
-    #     plt.savefig("%s/%s.png" % (figures_dir, name), bbox_inches="tight")
-    #     plt.close()
 
         make_ki_plot(df_all_k_pars, name + "_k_i", figures_dir)
 
